de8bf5c5a0d761295c13226f678c4aa7e53ad32c
banna <379334810@qq.com>
2016-03-28 09:33:26 +0800

    修改在保存预测信息时的后台方法对于时间参数的接收方式，修改为字符串接收 修改在获取预测信息列表数据时，若是在添加公告的预测信息时，可以根据公告的有效期对预测信息进行筛选

--- parentproject/usermgr/src/main/java/com/sdf/manager/notice/controller/ForecastController.java
+++ parentproject/usermgr/src/main/java/com/sdf/manager/notice/controller/ForecastController.java
@@ -1,8 +1,8 @@
 package com.sdf.manager.notice.controller;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -94,6 +94,9 @@ public class ForecastController //extends GlobalExceptionHandler
 				@RequestParam(value="lottertyType",required=false) String lottertyType,//彩种
 				@RequestParam(value="province",required=false) String province,//省份
 				@RequestParam(value="city",required=false) String city,//市
+				@RequestParam(value="noticeData",required=false) String noticeData,//noticeData:1,获取的是应用公告中的预测信息数据
+				@RequestParam(value="startTime",required=false) String startTime,
+				@RequestParam(value="endTime",required=false) String endTime,//应用广告的有效结束时间
 				ModelMap model,HttpSession httpSession) throws Exception
 		{
 		 	Map<String,Object> returnData = new HashMap<String,Object> ();
@@ -126,6 +129,30 @@ public class ForecastController //extends GlobalExceptionHandler
 			{
 				params.add(city);
 				buffer.append(" and appAreaCity = ?").append(params.size());
+			}
+			
+			if(null!=noticeData&&"1".equals(noticeData))//获取的若是应用公告的预测数据，则要显示
+			{
+				Date st =  DateUtil.formatStringToDate(startTime.toString(), DateUtil.SIMPLE_DATE_FORMAT);//将前台传过来的字符串日期转换为date时，使用的格式应该与接收的字符串一样，即：若2014-02-01则应该用DateUtil.SIMPLE_DATE_FORMAT，在date和时间戳之间可以跨越格式转换
+				
+				
+				Date et =  DateUtil.formatStringToDate(endTime.toString(), DateUtil.SIMPLE_DATE_FORMAT);
+				
+				
+				if(null != startTime&& !"".equals(startTime.trim()) )//
+				{
+					params.add(DateUtil.formatDateToTimestamp(st,  DateUtil.FULL_DATE_FORMAT));
+					buffer.append(" and startTime >= ?").append(params.size());
+					//且开始时间也要小于结束时间
+					params.add(DateUtil.formatDateToTimestamp(et,  DateUtil.FULL_DATE_FORMAT));
+					buffer.append(" and startTime <= ?").append(params.size());
+				}
+				
+				if(null != endTime && !"".equals(endTime.trim()))//
+				{
+					params.add(DateUtil.formatDateToTimestamp(et,  DateUtil.FULL_DATE_FORMAT));
+					buffer.append(" and endTime >= ?").append(params.size());
+				}
 			}
 		 	
 			//排序
@@ -162,8 +189,8 @@ public class ForecastController //extends GlobalExceptionHandler
 				@RequestParam(value="lotteryType",required=false) String lotteryType,
 				@RequestParam(value="appAreaProvince",required=false) String appAreaProvince,
 				@RequestParam(value="appAreaCity",required=false) String appAreaCity,
-				@RequestParam(value="startTime",required=false) Date startTime,
-				@RequestParam(value="endTime",required=false) Date endTime,//应用的默认单价
+				@RequestParam(value="startTime",required=false) String startTime,
+				@RequestParam(value="endTime",required=false) String endTime,//应用的默认单价
 				ModelMap model,HttpSession httpSession) throws Exception
 		{
 		   ResultBean resultBean = new ResultBean ();
@@ -178,8 +205,10 @@ public class ForecastController //extends GlobalExceptionHandler
 			   forecastMessage.setLotteryType(lotteryType);
 			   forecastMessage.setAppAreaProvince(appAreaProvince);
 			   forecastMessage.setAppAreaCity(appAreaCity);
-			   forecastMessage.setStartTime(DateUtil.formatDateToTimestamp(startTime, DateUtil.FULL_DATE_FORMAT));
-			   forecastMessage.setEndTime(DateUtil.formatDateToTimestamp(endTime, DateUtil.FULL_DATE_FORMAT));
+			   Date st = DateUtil.formatStringToDate(startTime, DateUtil.SIMPLE_DATE_FORMAT);
+			   forecastMessage.setStartTime(DateUtil.formatDateToTimestamp(st, DateUtil.FULL_DATE_FORMAT));
+			   Date et = DateUtil.formatStringToDate(endTime, DateUtil.SIMPLE_DATE_FORMAT);
+			   forecastMessage.setEndTime(DateUtil.formatDateToTimestamp(et, DateUtil.FULL_DATE_FORMAT));
 			   forecastMessage.setModify(LoginUtils.getAuthenticatedUserCode(httpSession));
 			   forecastMessage.setModifyTime(new Timestamp(System.currentTimeMillis()));
 			   
@@ -200,8 +229,10 @@ public class ForecastController //extends GlobalExceptionHandler
 			   forecastMessage.setLotteryType(lotteryType);
 			   forecastMessage.setAppAreaProvince(appAreaProvince);
 			   forecastMessage.setAppAreaCity(appAreaCity);
-			   forecastMessage.setStartTime(DateUtil.formatDateToTimestamp(startTime, DateUtil.FULL_DATE_FORMAT));
-			   forecastMessage.setEndTime(DateUtil.formatDateToTimestamp(endTime, DateUtil.FULL_DATE_FORMAT));
+			   Date st = DateUtil.formatStringToDate(startTime, DateUtil.SIMPLE_DATE_FORMAT);
+			   forecastMessage.setStartTime(DateUtil.formatDateToTimestamp(st, DateUtil.FULL_DATE_FORMAT));
+			   Date et = DateUtil.formatStringToDate(endTime, DateUtil.SIMPLE_DATE_FORMAT);
+			   forecastMessage.setEndTime(DateUtil.formatDateToTimestamp(et, DateUtil.FULL_DATE_FORMAT));
 			   forecastMessage.setModify(LoginUtils.getAuthenticatedUserCode(httpSession));
 			   forecastMessage.setModifyTime(new Timestamp(System.currentTimeMillis()));
 			   forecastMessage.setCreater(LoginUtils.getAuthenticatedUserCode(httpSession));
