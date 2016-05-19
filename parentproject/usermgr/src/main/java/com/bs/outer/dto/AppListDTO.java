be6110ecd5f2eecde627201898085cff32951c86
banna <379334810@qq.com>
2016-05-19 14:11:19 +0800

    添加app的返回dto 添加通行证的接口dto

---
+++ parentproject/usermgr/src/main/java/com/bs/outer/dto/AppListDTO.java
@@ -0,0 +1,28 @@
+package com.bs.outer.dto;
+
+public class AppListDTO {
+	
+	private String appName;
+	
+	private String appId;
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	
+	
+	
+
+}
